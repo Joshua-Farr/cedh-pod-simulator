c79c33daf66d75d96b93b2f4af6cc97d17816275
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2024-05-22 18:22:05 -0500

    Added local storage functionality (#18)

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,18 +1,25 @@
 import { Commander } from "../types/types";
 
-const saveToLocalStorage = (data: object | Commander) => {
+const saveToLocalStorage = (data: Commander) => {
   window.localStorage.setItem("commanderdata", JSON.stringify(data));
   console.log("Data saved successfully to local storage");
 };
 
-const retrieveFromLocalStorage = () => {
+const retrieveFromLocalStorage = (): Commander => {
   const data = window.localStorage.getItem("commanderdata");
-  let savedData = "";
+  let savedData: Commander = {
+    commander: "",
+    decklist: [],
+    currentCommanders: undefined,
+    hand: [],
+  };
+
   if (data) {
     savedData = JSON.parse(data);
   }
   console.log("Data retrieved successfully from local storage");
-  return savedData;
+  const newCommanderData = { ...savedData };
+  return newCommanderData;
 };
 
 export { saveToLocalStorage, retrieveFromLocalStorage };
